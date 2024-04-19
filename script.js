e78ff666a549fb06252d0016e9b15dffaf3a4742
noamp09 <noamp09@gmail.com>
2024-04-19 19:57:47 +0300

    game logic polishing

--- script.js
+++ script.js
@@ -19,15 +19,31 @@ function setupInput() {
 async function handleInput(e) {
     switch (e.key) {
         case "ArrowUp":
+            if (!canMoveUp()){
+                setupInput()
+                return
+            }
             await moveUp()
             break
         case "ArrowDown":
+            if (!canMoveDown()){
+                setupInput()
+                return
+            }
             await moveDown()
             break
         case "ArrowLeft":
+            if (!canMoveLeft()){
+                setupInput()
+                return
+            }
             await moveLeft()
             break
         case "ArrowRight":
+            if (!canMoveRight()){
+                setupInput()
+                return
+            }
             await moveRight()
             break
         default:
@@ -37,6 +53,15 @@ async function handleInput(e) {
 
     grid.cells.forEach(cell => cell.mergeTiles())
 
+    const newTile = new Tile(gameboard)
+    grid.randomEmptyCell().tile = newTile
+
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+        newTile.waitForTransition(true).then(() => {
+            alert("You Lose")
+        })
+        return
+    }
     setupInput()
 }
 
@@ -82,4 +107,31 @@ function sildeTiles(cells) {
         }
         return promises
     }))
+}
+
+function canMoveUp() {
+    return canMove(grid.cellsByColumn)
+}
+
+function canMoveDown() {
+    return canMove(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+
+function canMoveLeft() {
+    return canMove(grid.cellsByRow)
+}
+
+function canMoveRight() {
+    return canMove(grid.cellsByRow.map(row => [...row].reverse()))
+}
+
+function canMove(cells) {
+    return cells.some(group => {
+        return group.some((cell, index) => {
+            if (index === 0) return false
+            if (cell.tile == null) return false
+            const moveToCell = group[index - 1]
+            return moveToCell.canAccept(cell.tile)
+        })
+    })
 }
